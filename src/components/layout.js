e0e9e6862a67862b327d4dc869994c18bb71c6c4
Jhonattan <jhonattan.benitez@hotmail.com>
2019-01-17 04:16:30 -0500

    sass added, tags team and about pages added

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import '../styles/index.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,19 +19,10 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+        <div className="container" id="content">
           {children}
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+                      © {new Date().getFullYear()}, Max {` `} Tigers
           </footer>
         </div>
       </>
