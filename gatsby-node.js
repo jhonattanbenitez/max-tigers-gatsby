70ad443f4ed6210a0019aaba58be1b31b441a2dd
Jhonattan <jhonattan.benitez@hotmail.com>
2019-01-18 15:02:43 -0500

    created pagination pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -21,7 +21,8 @@ exports.createPages = ({ actions, graphql }) => {
     const templates = {
         singlePost: path.resolve('src/templates/single-post.js'),
         tagsPage: path.resolve('src/templates/tags-page.js'),
-        tagPosts: path.resolve('src/templates/tag-post.js')
+        tagPosts: path.resolve('src/templates/tag-post.js'),
+        postList: path.resolve('src/templates/post-list.js')
     }
     return graphql(`
         {
@@ -89,6 +90,23 @@ exports.createPages = ({ actions, graphql }) => {
                     tag,
 
                 },
+            })
+        })
+        const postsPerPage = 2;
+        const numberOfPages = Math.ceil(posts.length / postsPerPage);
+
+        Array.from({ length: numberOfPages }).forEach(( _, index) => {
+            const isFirstPage = index === 0
+            const currentPage = index + 1 
+            if (isFirstPage) return
+            createPage({
+                path: `/page/${currentPage}`,
+                component: templates.postList,
+                context: {
+                    limit: postsPerPage,
+                    skip: index * postsPerPage,
+                    currentPage
+                }
             })
         })
     })
--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,8 @@ const IndexPage = () => (
 )
 const indexQuery = graphql`
 {
-  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}
+  limit: 2) {
     edges {
       node {
         id
