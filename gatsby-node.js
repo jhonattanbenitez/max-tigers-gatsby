12c3a28e11f7129e5dd1b5e98de7d3e92a2ef6e9
Jhonattan <jhonattan.benitez@hotmail.com>
2019-01-18 21:23:24 -0500

    webpack windows

--- gatsby-node.js
+++ gatsby-node.js
@@ -123,3 +123,18 @@ exports.createPages = ({ actions, graphql }) => {
         })
     })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /bad-module/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
