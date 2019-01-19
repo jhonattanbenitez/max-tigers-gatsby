4cff4bfba7b5e494cdcd48b48bc2291d4b7729a3
Jhonattan <jhonattan.benitez@hotmail.com>
2019-01-19 01:50:10 -0500

    twitch removes

--- src/pages/streams.js
+++ src/pages/streams.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import ReactTwitchEmbedVideo from 'react-twitch-embed-video'
+
 
 const Streams = () => (
   <Layout pageTitle="Streams">
     <SEO title="Streams" keywords={[`gatsby`, `application`, `react`]} />
-    <ReactTwitchEmbedVideo channel="Maxtigersgg" />
+    
   </Layout>
 )
 
